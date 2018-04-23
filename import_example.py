cda0ecafa9eaf7f5e53ad33393093f75eb5ee181
Greg <ghunkins@users.noreply.github.com>
2018-04-22 21:13:33 -0400

    more interpolate

--- import_example.py
+++ import_example.py
@@ -25,7 +25,7 @@ for idx, ratio in enumerate(np.linspace(0, 1, 10)):
 	z1 = np.stack([interpolate.slerp(ratio, r1, r2) for r1, r2 in zip(chosen_latents[0], chosen_latents[1])])
 	z2 = np.stack([interpolate.slerp(ratio, r1, r2) for r1, r2 in zip(chosen_latents[0], chosen_latents[2])])
 	#z3 = np.stack([interpolate.slerp(ratio, r1, r2) for r1, r2 in zip(chosen_latents[1], chosen_latents[2])])
-	latents.append(z1)
+	latents.append(z2)
 	#latents.append(z2)
 
 latents = np.array(latents)
